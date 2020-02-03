0ea932a6876a759c2565b890c5bfb2b3ee80450f
Qbolewicki <Qbolewicki@icloud.com>
2020-02-02 22:40:49 -0500

    quinn edits 2-2-20

---
+++ tutorial1_intro/main.js
@@ -0,0 +1,43 @@
+// load in csv
+d3.csv("../../data/homework-hours.csv").then(data => {
+    // once the data loads, console log it
+    console.log("data", data);
+  
+    // select the `table` container in the HTML
+    const table = d3.select("#d3-table");
+  
+    /** HEADER */
+    const thead = table.append("thead");
+    thead
+      .append("tr")
+      .append("th")
+      .attr("colspan", "7")
+      .text("Week One Results in Hours");
+  
+    thead
+      .append("tr")
+      .selectAll("th")
+      .data(data.columns)
+      .join("td")
+      .text(d => d);
+  
+    /** BODY */
+    // rows
+    const rows = table
+      .append("tbody")
+      .selectAll("tr")
+      .data(data)
+      .join("tr");
+  
+    // cells
+    rows
+      .selectAll("td")
+      .data(d => Object.values(d))
+      .join("td")
+      // update the below logic to apply to your dataset
+      
+      .attr("class", d => +d < 3 ? 'low' : 'high')
+      .append("class", d => +d > 3 ? 'high' : 'low')
+      .text(d => d);
+  });
+  
